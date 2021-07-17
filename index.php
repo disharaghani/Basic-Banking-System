87b61a5e58a2adf7699685ecf539bb1d132faad5
Disha Raghani <71838485+disharaghani@users.noreply.github.com>
2021-07-17 19:57:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<link rel="stylesheet" href="node_modules/mdbootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="node_modules/mdbootstrap/css/mdb.min.css">
+<link rel="stylesheet" href="node_modules/mdbootstrap/css/style.css">
+    <title>The Sparks Bank</title>
+</head>
+
+<body>
+    
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">The Sparks Bank</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="customer.php">View Customers</a>
+                        <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+                        <a class="nav-link" href="transactionhistory.php">View Transaction History</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <img src="./assets/tsf.png" alt="">
+    <h1 style="display:inline;padding-left: 150px;font-size:70px">THE SPARKS FOUNDATION</h1>
+    <h1 style="font-style: italic;">Web Development & Designing Internship(GRIPJULY21)</h1>
+    <h1 style="font-style: italic;">Basic Banking System</h1>
+    <!-- <h3 style="text-align: center;">Done By<h3> -->
+    <br>
+    <h2 style="text-align: center;font-style:italic">Done By:-Chirag Dayaramani</h2>
+    
+    <!-- <h1 style="display:inline;">Internship</h1> -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3" style="text-align: center;">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+          <!-- Twitter -->
+          <a class="tw-ic" href="https://twitter.com/chirag_24_">
+            <i class="fab fa-twitter fa-3x white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <!-- Google -->
+          <a class="btn-floating btn-lg fa-3x btn-email" type="button" role="button" href="mailto: 2019chirag.dayaramani@ves.ac.in">
+            <i class="fas fa-envelope"></i></a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <!--Linkedin -->
+          <a class="li-ic" href="https://www.linkedin.com/in/chirag-dayaramani-b8065b19a/">
+            <i class="fab fa-linkedin-in fa-3x white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <!--Github-->
+        <a class="btn-floating btn-lg fa-3x btn-git" type="button" role="button" href="https://github.com/chiragdayaramani">
+            <i class="fab fa-github"></i>
+        </a>
+
+        </div>
+        © 2021 Copyright:  Disha Raghani
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <!-- <div class="footer-copyright text-center py-3"> -->
+  <!-- © 2021 Copyright:  Disha Raghani -->
+  <!-- </div> -->
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+</html>
---
+++ transactionhistory.php
@@ -0,0 +1,84 @@
+<?php
+ 
+	session_start();
+	include 'config.php';
+
+	$q="select * from transactions";
+	$result=mysqli_query($con,$q);
+	$row_count=mysqli_num_rows($result);
+	
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Transaction History</title>
+</head>
+
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">The Sparks Bank</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link " href="customer.php">View Customers</a>
+                        <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+                        <a class="nav-link active" href="transactionhistory.php">View Transaction History</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <h1 style="color:white;">Transaction History</h1>
+    <body style="background-color:#1B3F8B;">
+
+    <br>	  
+    <!-- <h2 style="color:white;text-align: center;font-family: cursive;">Customer Details</h2> -->
+
+    <table class="table table-success table-striped table-hover">
+        <thead>
+            <th>SENDER NAME</th>
+            <th>RECEIVER NAME</th>
+            <th>AMOUNT</th>	
+		</thead>
+		<tbody>
+		<tr text-align = center>		
+			<?php  while($row=mysqli_fetch_array($result)){ ?>		
+                <td><?php echo  $row["Sender"]; ?></td>
+                <td><?php echo  $row["Receiver"]; ?></td>
+                <td><?php echo  $row["Amount"]; ?></td>
+		</tr>
+	        <?php } ?>
+		</tbody>
+    </table>
+
+	
+	
+</body>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
